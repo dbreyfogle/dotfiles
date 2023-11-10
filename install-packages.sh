8abc96c001f7466163a7fc4120ec86bf96d8a397
Danny Breyfogle <dwbreyfogle@gmail.com>
2023-11-10 16:04:16 -0700

    cleanup install packages

--- install-packages.sh
+++ install-packages.sh
@@ -1,6 +1,27 @@
 #!/bin/sh
 sudo apt update
-sudo apt install -y curl git tmux xsel xclip vim-gtk3 gparted htop cifs-utils openssh-server openvpn virt-manager spice-client-gtk krdc krfb borgbackup borgmatic timeshift flameshot filelight
+sudo apt install -y \
+    curl \
+    git \
+    tmux \
+    xsel \
+    xclip \
+    vim-gtk3 \
+    build-essential \
+    gparted \
+    htop \
+    cifs-utils \
+    openssh-server \
+    openvpn \
+    virt-manager \
+    spice-client-gtk \
+    krdc \
+    krfb \
+    borgbackup \
+    borgmatic \
+    timeshift \
+    flameshot \
+    filelight
 
 # GitHub CLI
 type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
@@ -11,10 +32,10 @@ curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo 
 && sudo apt install gh -y
 
 # Prep for Docker Desktop
-sudo apt remove docker-desktop
+sudo apt remove -y docker-desktop
 rm -r $HOME/.docker/desktop
 sudo rm /usr/local/bin/com.docker.cli
-sudo apt purge docker-desktop
+sudo apt purge -y docker-desktop
 sudo apt install -y gnome-terminal
 
 # Add Docker's official GPG key:
@@ -32,27 +53,23 @@ echo \
 sudo apt-get update
 
 # Docker Desktop
-wget -O /tmp/docker-desktop.deb https://desktop.docker.com/linux/main/amd64/docker-desktop-4.25.0-amd64.deb?utm_source=docker&utm_medium=webreferral&utm_campaign=docs-driven-download-linux-amd64
+wget -O /tmp/docker-desktop.deb 'https://desktop.docker.com/linux/main/amd64/docker-desktop-4.25.0-amd64.deb?utm_source=docker&utm_medium=webreferral&utm_campaign=docs-driven-download-linux-amd64'
 sudo apt-get install -y /tmp/docker-desktop.deb
 
 # VSCode
-wget -O /tmp/vscode.deb https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64
+wget -O /tmp/vscode.deb 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64'
 sudo apt install -y /tmp/vscode.deb
 
 # Obsidian
-wget -O /tmp/obsidian.deb https://github.com/obsidianmd/obsidian-releases/releases/download/v1.4.16/obsidian_1.4.16_amd64.deb
+wget -O /tmp/obsidian.deb 'https://github.com/obsidianmd/obsidian-releases/releases/download/v1.4.16/obsidian_1.4.16_amd64.deb'
 sudo apt install -y /tmp/obsidian.deb
 
 # Discord
-wget -O /tmp/discord.deb https://discord.com/api/download?platform=linux&format=deb
+wget -O /tmp/discord.deb 'https://discord.com/api/download?platform=linux&format=deb'
 sudo apt install -y /tmp/discord.deb
 
-# Google Chrome
-wget -O /tmp/google-chrome.deb https://www.google.com/chrome/next-steps.html?statcb=0&installdataindex=empty&defaultbrowser=0#
-sudo apt install -y /tmp/google-chrome.deb
-
 # Bitwarden
-wget -O /tmp/bitwarden.deb https://vault.bitwarden.com/download/?app=desktop&platform=linux&variant=deb
+wget -O /tmp/bitwarden.deb 'https://vault.bitwarden.com/download/?app=desktop&platform=linux&variant=deb'
 sudo apt install -y /tmp/bitwarden.deb
 
 # pyenv
@@ -60,6 +77,3 @@ curl https://pyenv.run | bash
 
 # n
 curl -L https://bit.ly/n-install | bash -s -- -y
-
-# tldr
-npm install -g tldr
